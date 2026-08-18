e6bf23421f7c1c9392ad416f82f7ba90f839db0e
Gabrielo1212 <gabodrec2@gmail.com>
2026-08-17 20:00:54 -0400

    Trabajo en Clases 1. SOquets TCP Protocolo

---
+++ Trabajo en Clases/Trabajo en Clases 1. SOquets TCP Protocolo/operaciones/1protocolo/server-operacion/src/main/java/com/sis258/server/operacion/ServerOperacion.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sis258.server.operacion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerOperacion {
+
+    public static void main(String[] args) {
+        int port = 5002;
+        ServerSocket server;
+        
+        try {
+            server = new ServerSocket(port);
+            while (true) {
+                
+                System.out.println("Se inicio el servidor con éxito");
+                Socket client;
+                PrintStream toClient;
+                client = server.accept(); 
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+                System.out.println("Cliente se conecto");
+                String recibido = fromClient.readLine();
+                System.out.println("El cliente envio el mensaje:" + recibido);
+                toClient = new PrintStream(client.getOutputStream());
+                String respuesta = procesarSolicitud(recibido);
+                toClient.println(respuesta);
+                
+                client.close();
+
+            } 
+        }catch (IOException ex) {
+                System.out.print(ex.getMessage());
+        }
+    }
+
+    public static String procesarSolicitud(String cadena) {
+        try {
+            // SUM-5-10
+            String[] partes = cadena.split("-");
+            String operacion = partes[0].toUpperCase();
+            int num1 = Integer.parseInt(partes[1]);
+            int num2 = Integer.parseInt(partes[2]);
+            int resultado = 0;
+
+            switch (operacion) {
+                case "SUM": 
+                    resultado = num1 + num2; 
+                    break;
+                    
+                    
+                case "RES": 
+                    resultado = num1 - num2; 
+                    break;
+                    
+                case "MUL": 
+                    resultado = num1 * num2; 
+                    break;
+                    
+                case "DIV": 
+                    if (num2 != 0) 
+                        resultado = num1 / num2;
+                    else 
+                        return "Error: Division por cero";
+                    break;
+                default: 
+                    return "Operacion no valida";
+            }
+            
+            
+            return String.valueOf(resultado);
+            
+            
+            
+        } catch (Exception e) {
+            return "Error en el formato del protocolo";
+        }
+    }
+}
---
+++ Trabajo en Clases/Trabajo en Clases 1. SOquets TCP Protocolo/operaciones/2interactivo/server-operacion/src/main/java/com/sis258/server/operacion/ServerOperacion.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sis258.server.operacion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Dell
+ */
+public class ServerOperacion {
+
+    public static void main(String[] args) {
+        int port = 5003;
+        ServerSocket server;
+        try {
+            server = new ServerSocket(port);
+            while (true) {
+                
+                System.out.println("Se inicio el servidor con éxito");
+                Socket client;
+                PrintStream toClient;
+                client = server.accept(); 
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+                System.out.println("Cliente se conecto");
+                String recibido = fromClient.readLine();
+                System.out.println("El cliente envio el mensaje:" + recibido);
+                int numero1 = Integer.parseInt(recibido);
+                
+                toClient = new PrintStream(client.getOutputStream());
+                
+                toClient.println("introduzca el segundo numero");
+                String recibido2 = fromClient.readLine();
+                int numero2 = Integer.parseInt(recibido2);
+                
+                toClient.println("1.suma 2.resta 3. multiplicacion 4, division .introduzca la operacion");
+                String recibido3 = fromClient.readLine();
+                String respuesta;
+
+                switch (recibido3.toLowerCase()) {
+                    case "suma": case "1":
+                        respuesta = String.valueOf(numero1 + numero2);
+                        break;
+                        
+                    case "resta": case "2":
+                        respuesta = String.valueOf(numero1 - numero2);
+                        break; 
+                        
+                    case "multiplicacion": case "3":
+                        respuesta = String.valueOf(numero1 * numero2);
+                        break;
+                        
+                    case "division": case "4":
+                        if (numero2 != 0) {
+                            respuesta = String.valueOf(numero1 / numero2);
+                        } else {
+                            respuesta = "Error: No se puede dividir entre 0";
+                        }
+                        break;
+                        
+                    default:
+                        respuesta = "Operacion no valida";
+                        break;
+                }
+
+                
+                toClient.println(respuesta);
+
+                client.close();
+            } 
+        }catch (IOException ex) {
+                System.out.print(ex.getMessage());
+            }
+    }
+}
